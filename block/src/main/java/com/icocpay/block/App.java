926313839df4a7caf00aef81690cf7591ed15389
icocpay <icocpay@gmail.com>
2018-07-29 10:32:43 +0800

    Initial commit

---
+++ block/src/main/java/com/icocpay/block/App.java
@@ -0,0 +1,13 @@
+package com.icocpay.block;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
